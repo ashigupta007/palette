29ebc5cae99d4c306f24c9b8236e392aa80e59d6
Ashish Gupta <akvns152@gmail.com>
2018-06-25 00:27:15 +0530

    basic frontend added

---
+++ pages/home.php
@@ -0,0 +1,70 @@
+<html>
+<head>
+	<title>
+		Palette
+	</title>
+
+	<meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="theme-color" content="#1E90FF">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="manifest" href="../manifest.json">
+
+    <link rel="icon" type="image/png" href="../images/icon.png">
+</head>
+
+<body>
+
+	<div class="row">
+		<div class="col-sm-4">
+			<div class="menu">
+				<div class="header">
+					<img src="images/icon.png">
+					<h1>Palette</h1>
+					<div class="dropdown">
+						<button class="dropdown-toggle" data-toggle="dropdown">
+							<i class="fas fa-ellipsis-v"></i>
+						</button>
+						<ul class="dropdown-menu dropdown-menu-right">
+	    					<li><a href="#">Profile</a></li>
+	    					<li><a href="#">Logout</a></li>
+	    					<li><a href="#">Help?</a></li>
+	  					</ul>
+  					</div>
+				</div>
+				<ul class="menu-cont">
+					<li class="menu-item add">
+						<button class="add-icon">+</button>	Add Project
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-sm-8">
+			
+		</div>
+	</div>
+
+</body>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+
+    <script type="text/javascript" src="../js/app.js"></script>
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Qwigley" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+</html>
+
+</html>
